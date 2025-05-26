4b27f03d4090a1736098badbfff3cc40103cd4c8
Sparse808 <sparse.dev@gmail.com>
2025-05-25 22:53:12 -0400

    got movable objects

--- hello.go
+++ hello.go
@@ -1,14 +1,120 @@
 package main
 
 import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+
+	"fmt"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("Draggable Rectangle")
+	gridBackground := canvas.NewRectangle(color.Black)
+	gridBackground.Resize(fyne.NewSize(100, 300))
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	rect := NewDraggableRect(50, 50, *gridBackground)
+
+	noLayout := container.NewWithoutLayout(gridBackground, rect)
+
+	title := widget.NewLabel("Tools")
+	but1 := widget.NewButton("box", func() {
+		fmt.Println("pressed")
+
+		noLayout.Objects = append(noLayout.Objects, NewDraggableRect(70, 70, *gridBackground))
+	})
+
+	toolsSection := container.NewVBox(title, but1)
+
+	myLayout := container.NewHBox(layout.NewSpacer(), noLayout, layout.NewSpacer(), toolsSection)
+
+	w.SetContent(myLayout)
+	w.Resize(fyne.NewSize(700, 500))
 	w.ShowAndRun()
 }
+
+type DraggableRect struct {
+	widget.BaseWidget
+	rect        *canvas.Rectangle
+	gridGrounds canvas.Rectangle
+}
+
+func NewDraggableRect(x, y float32, grounds canvas.Rectangle) *DraggableRect {
+	dr := &DraggableRect{
+		rect:        canvas.NewRectangle(color.NRGBA{R: 255, G: 100, B: 100, A: 255}),
+		gridGrounds: grounds,
+	}
+	dr.ExtendBaseWidget(dr)
+	dr.Resize(fyne.NewSize(80, 60))
+	dr.Move(fyne.NewPos(x, y))
+	return dr
+}
+
+// Renderer
+func (r *DraggableRect) CreateRenderer() fyne.WidgetRenderer {
+	// The rectangle is always positioned at (0,0) inside the widget
+	return &draggableRectRenderer{
+		rect:    r,
+		objects: []fyne.CanvasObject{r.rect},
+	}
+}
+
+type draggableRectRenderer struct {
+	rect    *DraggableRect
+	objects []fyne.CanvasObject
+}
+
+func (r *draggableRectRenderer) Layout(size fyne.Size) {
+	// Fill the entire widget space
+	r.rect.rect.Resize(size)
+	r.rect.rect.Move(fyne.NewPos(0, 0))
+}
+
+func (r *draggableRectRenderer) MinSize() fyne.Size {
+	return r.rect.Size()
+}
+
+func (r *draggableRectRenderer) Refresh() {
+	r.rect.rect.FillColor = color.NRGBA{R: 255, G: 100, B: 100, A: 255}
+	r.rect.rect.Refresh()
+}
+
+func (r *draggableRectRenderer) BackgroundColor() color.Color {
+	return color.Transparent
+}
+
+func (r *draggableRectRenderer) Objects() []fyne.CanvasObject {
+	return r.objects
+}
+
+func (r *draggableRectRenderer) Destroy() {}
+
+// Dragged interface implementations
+
+func (r *DraggableRect) Dragged(e *fyne.DragEvent) {
+	y := r.gridGrounds.Position().Y + r.gridGrounds.Size().Height
+	x := r.gridGrounds.Position().X + r.gridGrounds.Size().Width
+
+	newPos := r.Position().Add(e.Dragged)
+
+	if newPos.X+r.Size().Width > x {
+		newPos.X = x - r.Size().Width
+	} else if newPos.X < r.gridGrounds.Position().X {
+		newPos.X = r.gridGrounds.Position().X
+	}
+	if newPos.Y+r.Size().Height > y {
+		newPos.Y = y - r.Size().Height
+	} else if newPos.Y < r.gridGrounds.Position().Y {
+		newPos.Y = r.gridGrounds.Position().Y
+	}
+
+	r.Move(newPos) // Move the widget itself
+}
+
+func (r *DraggableRect) DragEnd() {}
