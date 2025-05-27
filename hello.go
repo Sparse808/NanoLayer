629e641dc0876dc0ca20172150acec2c439748bb
Sparse808 <sparse.dev@gmail.com>
2025-05-26 21:50:02 -0400

    inspector set up

--- hello.go
+++ hello.go
@@ -16,23 +16,35 @@ import (
 func main() {
 	a := app.New()
 	w := a.NewWindow("Draggable Rectangle")
+
+	//screen background
 	gridBackground := canvas.NewRectangle(color.Black)
 	gridBackground.Resize(fyne.NewSize(100, 300))
 
-	rect := NewDraggableRect(50, 50, *gridBackground)
+	noLayout := container.NewWithoutLayout(gridBackground)
 
-	noLayout := container.NewWithoutLayout(gridBackground, rect)
+	//inspector section
+	inspectorTitle := widget.NewLabel("Inspector")
+	inspectedRect := widget.NewLabel("rect")
 
+	//side panel layout
+	toolsSection := container.NewVBox()
+	inspecterSection := container.NewVBox(inspectorTitle, inspectedRect)
+
+	//toolsSection
 	title := widget.NewLabel("Tools")
 	but1 := widget.NewButton("box", func() {
 		fmt.Println("pressed")
 
-		noLayout.Objects = append(noLayout.Objects, NewDraggableRect(70, 70, *gridBackground))
+		noLayout.Objects = append(noLayout.Objects, NewDraggableRect(70, 70, *gridBackground, inspectedRect))
 	})
 
-	toolsSection := container.NewVBox(title, but1)
+	toolsSection.Objects = append(toolsSection.Objects, title)
+	toolsSection.Objects = append(toolsSection.Objects, but1)
 
-	myLayout := container.NewHBox(layout.NewSpacer(), noLayout, layout.NewSpacer(), toolsSection)
+	sidePanel := container.NewHBox(layout.NewSpacer(), toolsSection, inspecterSection)
+
+	myLayout := container.NewHBox(layout.NewSpacer(), noLayout, layout.NewSpacer(), sidePanel)
 
 	w.SetContent(myLayout)
 	w.Resize(fyne.NewSize(700, 500))
@@ -43,12 +55,14 @@ type DraggableRect struct {
 	widget.BaseWidget
 	rect        *canvas.Rectangle
 	gridGrounds canvas.Rectangle
+	inspector   *widget.Label
 }
 
-func NewDraggableRect(x, y float32, grounds canvas.Rectangle) *DraggableRect {
+func NewDraggableRect(x, y float32, grounds canvas.Rectangle, inspector *widget.Label) *DraggableRect {
 	dr := &DraggableRect{
 		rect:        canvas.NewRectangle(color.NRGBA{R: 255, G: 100, B: 100, A: 255}),
 		gridGrounds: grounds,
+		inspector:   inspector,
 	}
 	dr.ExtendBaseWidget(dr)
 	dr.Resize(fyne.NewSize(80, 60))
@@ -94,6 +108,11 @@ func (r *draggableRectRenderer) Objects() []fyne.CanvasObject {
 }
 
 func (r *draggableRectRenderer) Destroy() {}
+
+func (r *DraggableRect) Tapped(e *fyne.PointEvent) {
+	fmt.Println(&r)
+	r.inspector.SetText(fmt.Sprintf("%p", r))
+}
 
 // Dragged interface implementations
 
